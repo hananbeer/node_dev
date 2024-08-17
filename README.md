37d0b6a65b2c532def1f87c250709338d36ea593
high_byte <email@hanan.beer>
2024-08-17 14:53:58 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# node_dev
-ComfyUI Node Development Helper
+# node_dev - ComfyUI Node Development Helper
+
+Browse to this endpoint to reload custom nodes for more streamlined development:
+
+http://127.0.0.1:8188/node_dev/reload/<module_name>
+
---
+++ __init__.py
@@ -0,0 +1,24 @@
+import re
+import importlib
+
+from aiohttp import web
+from server import PromptServer
+
+from nodes import load_custom_node
+
+routes = PromptServer.instance.routes
+
+@routes.get("/node_dev/reload/{module_name}")
+async def reload(request):
+    """ Reload the requested custom node """
+    module_name = request.match_info['module_name']
+    if not re.match('^[0-9A-Za-z_-]+$', module_name):
+        return web.Response(text='Invalid module name', status=400)
+
+    module = importlib.import_module(module_name)
+    # ensure the module is reloaded
+    importlib.reload(module)
+    load_custom_node('custom_nodes/' + module_name)
+    return web.Response(text='OK') #headers={'Location': '/'})
+
+
